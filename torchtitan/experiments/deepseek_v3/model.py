38d960b6aca58f12007d74cd86d6e0e23eb035de
PURAB SETH <PURABSETH@COLLEGE.HARVARD.EDU>
2025-07-25 18:39:12 -0700

    ?

--- torchtitan/experiments/deepseek_v3/model.py
+++ torchtitan/experiments/deepseek_v3/model.py
@@ -1,35 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-# This code is based on model definition of `deepseek-ai/DeepSeek-V3-Base` on
-# Hugging Face Model Hub. Url:
-# https://huggingface.co/deepseek-ai/DeepSeek-V3-Base/blob/main/modeling_deepseek.py
-# https://huggingface.co/deepseek-ai/DeepSeek-V3-Base/resolve/main/configuration_deepseek.py
-#
-# It has been modified from its original forms to accommodate naming convention
-# and usage patterns of the TorchTitan project.
-
-# Copyright 2023 DeepSeek-AI and The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 """ PyTorch DeepSeek model."""
-
-
 import math
 
 # from re import X
